8fb80ea3a1f54f1fd50d0dfe3ad8e2ea0662900e
iriteshp <iriteshp@users.noreply.github.com>
2022-06-11 20:50:06 +0000

    Add new client

--- terraform/applications/keycloak/modules/login-gov/openid_clients.tf
+++ terraform/applications/keycloak/modules/login-gov/openid_clients.tf
@@ -52,3 +52,26 @@ resource "keycloak_openid_client" "react_test_app" {
   valid_redirect_uris                        = var.react_test_app_uris
   web_origins                                = ["*"]
 }
+resource "keycloak_openid_client" "client-test1" {
+access_type                                = "PUBLIC"
+backchannel_logout_revoke_offline_sessions = "false"
+backchannel_logout_session_required        = "false"
+client_authenticator_type                  = "client-secret"
+client_id                                  = "client-test1"
+consent_required                           = "false"
+direct_access_grants_enabled               = "true"
+display_on_consent_screen                  = "false"
+enabled                                    = "true"
+frontchannel_logout_enabled                = "false"
+full_scope_allowed                         = "true"
+implicit_flow_enabled                      = "false"
+name                                       = "client-test1"
+oauth2_device_authorization_grant_enabled  = "false"
+realm_id                                   = keycloak_realm.login_gov_tf.id
+root_url                                   = var.react_test_app_root_url
+admin_url                                  = var.react_test_app_admin_url
+standard_flow_enabled = "true"
+use_refresh_tokens    = "true"
+valid_redirect_uris                        = var.react_test_app_uris
+web_origins           = ["*"]
+}
