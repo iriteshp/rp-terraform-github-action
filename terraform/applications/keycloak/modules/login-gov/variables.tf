47a179af4024c350fe114c3653f65a4a5e328713
iriteshp <iriteshp@users.noreply.github.com>
2022-07-15 03:38:39 +0000

    Add new client

--- terraform/applications/keycloak/modules/login-gov/variables.tf
+++ terraform/applications/keycloak/modules/login-gov/variables.tf
@@ -15,3 +15,8 @@ variable "react_test_app_root_url" {
 variable "react_test_app_admin_url" {
   type = string
 }
+variable "enable_react_test_app" {
+  type        = bool
+  description = "Whether environment should use the react_test_app client"
+  default     = false
+}
